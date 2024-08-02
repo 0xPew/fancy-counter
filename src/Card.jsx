9e0eb59c3f1a4b081c785384b18beb6e7bedd598
Pew <hi@pew.dev>
2024-08-02 18:52:31 +0800

    Add increase counter hotkey

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Count from "./Count";
 import ResetButton from "./ResetButton";
 import Title from "./Title";
@@ -7,6 +7,25 @@ import ButtonContainer from "./ButtonContainer";
 export default function Card() {
   const [count, setCount] = useState(0);
   const locked = count === 5 ? true : false;
+
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      if (event.code === "Space") {
+        const newCount = count + 1;
+        if (newCount > 5) {
+          setCount(5);
+          return;
+        }
+        setCount(newCount);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  }, [count]);
 
   return (
     <div className={`card ${locked && "card--limit"}`}>
