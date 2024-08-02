8b913236682ba84b1efa24711b6a36ca8d9d3ed4
Pew <hi@pew.dev>
2024-08-02 18:08:06 +0800

    Refactor count button

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import Count from "./Count";
-import CountButtons from "./CountButtons";
 import ResetButton from "./ResetButton";
 import Title from "./Title";
+import ButtonContainer from "./ButtonContainer";
 
 export default function Card() {
   const [count, setCount] = useState(0);
@@ -12,7 +12,7 @@ export default function Card() {
       <Title />
       <Count count={count} />
       <ResetButton setCount={setCount} />
-      <CountButtons setCount={setCount} />
+      <ButtonContainer setCount={setCount} />
     </div>
   );
 }
--- src/ResetButton.jsx
+++ src/ResetButton.jsx
@@ -1,14 +1,13 @@
 import { ResetIcon } from "@radix-ui/react-icons";
 
 export default function ResetButton({ setCount }) {
+  const handleClick = () => {
+    setCount(0);
+  };
+
   return (
     <button className="reset-btn">
-      <ResetIcon
-        onClick={() => {
-          setCount(0);
-        }}
-        className="reset-btn-icon"
-      />
+      <ResetIcon onClick={handleClick} className="reset-btn-icon" />
     </button>
   );
 }
